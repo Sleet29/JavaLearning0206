66ef1b970508bffc51cfc2ddbb8d85da003e9c4a
jhta <jhta@DESKTOP-OMLBP36>
2024-02-07 10:00:25 +0900

    24/02/07 reviewed

--- src/exercise3/CRUD.java
+++ src/exercise3/CRUD.java
@@ -3,14 +3,14 @@ package exercise3;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+//조회할 컬럼 선택과 해당 컬럼 값 입력 후 조회 결과 출력하는 프로그램
 public class CRUD {
-    // CRUD:컴퓨터 소프트웨어가 가지는 기본적인 데이터 처리 기능
-    // C(Create-insert), R(Read-select)
-    // U(Update-update), D(Delete-delete)
-    static String menus[] = { "사원번호", "사원이름", "직급", "매니저", "입사일자", "급여", "커미션", "부서번호", "종료" };
+	// CRUD:컴퓨터 소프트웨어가 가지는 기본적인 데이터 처리 기능
+	// C(Create-insert), R(Read-select)
+	// U(Update-update), D(Delete-delete)
+	static String menus[] = { "사원번호", "사원이름", "직급", "매니저", "입사일자", "급여", "커미션", "부서번호", "종료" };
 
-    
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int menu = menuselect(sc);
 		String search_word = input(menu, sc);
@@ -18,15 +18,15 @@ public class CRUD {
 		sc.close();
 	}
 
-    
-    private static void search(int menu, String search_word) {
+	private static void search(int menu, String search_word) {
 		DAO dao = new DAO();
 		ArrayList<Emp> list = dao.search(menu - 1, search_word);
 
 		if (list.isEmpty()) {
 			System.out.println("검색 결과가 없습니다.");
 		} else {
-			System.out.printf("%s\t%s\t%s\t\t%s\t%s\t\t%s\t%s\t%s\t\n", menus[0], menus[1], menus[2], menus[3],
+			System.out.printf("%s\t%s\t%s\t\t%s\t%s\t\t%s\t%s\t%s\t\n", 
+					menus[0], menus[1], menus[2], menus[3],
 					menus[4], menus[5], menus[6], menus[7]);
 			for (Emp s : list) {
 				System.out.println(s.toString());
@@ -34,76 +34,46 @@ public class CRUD {
 		}
 
 	}
-    /*
-    public static void main(String[] args) {
-        selectAll();
-    }
-    */
 
-    
-    public static int menuselect(Scanner sc) {
-		
-    	int i=0;
-    	for (String m : menus) {
-    		System.out.println(++i + "." + m);
-    	}
-    	
-    	System.out.print("조회할 컬럼을 선택하세요>");
-    	return inputNumber(sc);
+	private static int menuselect(Scanner sc) {
+		int i = 0;
+		for (String m : menus) {
+			System.out.println(++i + "." + m);
+		}
+
+		System.out.print("조회할 컬럼을 선택하세요>");
+		return inputNumber(sc);
+
 	}
-	
-    private static String input(int menu, Scanner sc) {
-    	String search_word = "";
-    	if (menu!=9) 
-    		search_word = searchData(sc,menu);
-    	return search_word;
-    }
-    
-    public static String searchData(Scanner sc,int menu) {
-    	System.out.println("조회할 "+menus[menu - 1] + "를(을) 입력하세요>");
-    	return sc.next();
-    }
-	
-	public static int inputNumber(Scanner sc) {
+
+	private static String input(int menu, Scanner sc) {
+		String search_word = "";
+		if (menu != 9)
+			search_word = searchData(sc, menu);
+		return search_word;
+	}
+
+	private static String searchData(Scanner sc, int menu) {
+		System.out.print("조회할 " + menus[menu - 1] + "를(을) 입력하세요>");
+		return sc.next();
+	}
+
+	private static int inputNumber(Scanner sc) {
 		int input = 0;
 		int lowNumber = 1;
-		int hiNumber = menus.length; // 9
+		int hiNumber = menus.length;// 9
 		while (true)
 			try {
 				input = Integer.parseInt(sc.nextLine());
-				if(input<=hiNumber && input >=lowNumber) {
+				if (input <= hiNumber && input >= lowNumber) {
 					break;
 				} else {
-					System.out.println(lowNumber + "~" + hiNumber + "사이의 숫자를 입력하세요>");
+					System.out.print(lowNumber + "~" + hiNumber + "사이의 숫자를 입력하세요>");
 				}
 			} catch (NumberFormatException e) {
 				System.out.print("숫자로 입력하세요>");
 			}
 		return input;
 	}
-    
-	
-	
-	
-    
-    private static void selectAll() {
-        DAO dao = new DAO();
-        ArrayList<Emp> list = dao.selectAll();
 
-        if (list.isEmpty()) {
-            System.out.println("검색 결과가 없습니다.");
-        } else {
-            System.out.printf("%-8s %-8s %-16s %-8s %-16s %-8s %-8s %-7s\n", menus[0], menus[1], menus[2], menus[3],
-                    menus[4], menus[5], menus[6], menus[7]);
-            for (int i = 0; i <= 80; i++)
-                System.out.print("=");
-
-            System.out.println();
-
-            for (Emp g : list) {
-                System.out.println(g.toString());
-            }
-        }
-    }
 }
-
--- src/exercise3/DAO.java
+++ src/exercise3/DAO.java
@@ -1,130 +1,91 @@
+//Statement 사용예
+
 package exercise3;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class DAO {
-    static final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";
-    static final String DB_URL = "jdbc:oracle:thin:@localhost:1521:xe";
-    static final String USER = "scott";
-    static final String PASS = "tiger";
 
-    public ArrayList<Emp> selectAll() {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        ArrayList<Emp> list = new ArrayList<>();
+	public ArrayList<Emp> search(int col_index, String search_word) {
+		ArrayList<Emp> list  = new ArrayList<Emp>();
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		String column_name[] = { "empno", "ename", "job", "mgr", "hiredate", 
+				                 "sal", "comm", "deptno" };
+		String sql = "select * from emp";
+		
+		if (col_index != 8) {// 8은 종료를 의미
+			
+			String single = "";
+			
+			// ename, job, hiredate는 ' 필요
+			if (col_index == 1 || col_index == 2 || col_index == 4)
+				single = "'";
 
-        try {
-            Class.forName(JDBC_DRIVER);
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			sql = sql + " where " + column_name[col_index] + "=" 
+			     + single + search_word + single;
 
-            String sql = "SELECT * FROM emp";
-            pstmt = conn.prepareStatement(sql);
-            rs = pstmt.executeQuery();
+		}
+		
+		System.out.println("\n" + sql.toString() + "\n");		
+		
+		try {
+			// 1단계 : JDBC 드라이버를 로드한다.
+			String driver = "oracle.jdbc.driver.OracleDriver";
+			Class.forName(driver);
 
-            while (rs.next()) {
-                int empno = rs.getInt("empno");
-                String ename = rs.getString("ename");
-                String job = rs.getString("job");
-                int mgr = rs.getInt("mgr");
-                String hiredate = rs.getString("hiredate"); 
-                int sal = rs.getInt("sal");
-                int comm= rs.getInt("comm");
-                int deptno = rs.getInt("deptno");
-                
-                Emp emp = new Emp(empno, ename, job, mgr, hiredate, sal, comm, deptno); 
-                list.add(emp);
-            }
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (rs != null) rs.close();
-                if (pstmt != null) pstmt.close();
-                if (conn != null) conn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return list;
-    }
+			// 2단계 : DB에 연결한다.
+			String url = "jdbc:oracle:thin:@localhost:1521:xe";
+			conn = DriverManager.getConnection(url, "scott", "tiger");
 
-    public ArrayList<Emp> search(int menu, String searchWord) {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        ArrayList<Emp> list = new ArrayList<>();
+			// Statement 객체 얻기
+			stmt = conn.createStatement();
 
-        try {
-            Class.forName(JDBC_DRIVER);
-            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			// 쿼리 실행
+			rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+					
+				Emp st = new Emp();
+				st.setEmpno(rs.getInt(1));// 조회 결과 첫번째 컬럼 값을 가져옵니다.
+											// SQL의 결과 null인 경우 0을 리턴
+				st.setEname(rs.getString(2));
+				st.setJob(rs.getString(3));
+				st.setMgr(rs.getInt(4));
+				st.setHiredate(rs.getDate(5));
+				st.setSal(rs.getInt(6));
+				st.setComm(rs.getInt(7));
+				st.setDeptno(rs.getInt(8));
+				list.add(st);
+			}
 
-            String columnName = "";
-            switch (menu) {
-                case 0:
-                    columnName = "empno";
-                    break;
-                case 1:
-                    columnName = "ename";
-                    break;
-                case 2:
-                    columnName = "job";
-                    break;
-                case 3:
-                    columnName = "mgr";
-                    break;
-                case 4:
-                    columnName = "hiredate";
-                    break;
-                case 5:
-                    columnName = "sal";
-                    break;
-                case 6:
-                    columnName = "comm";
-                    break;
-                case 7:
-                    columnName = "deptno";
-                    break;
-                default:
-                    break;
-            }
-
-            String sql = "SELECT * FROM emp WHERE " + columnName + "=?";
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, searchWord);
-            rs = pstmt.executeQuery();
-
-            while (rs.next()) {
-                int empno = rs.getInt("empno");
-                String ename = rs.getString("ename");
-                String job = rs.getString("job");
-                int mgr = rs.getInt("mgr");
-                String hiredate = rs.getString("hiredate"); 
-                int sal = rs.getInt("sal");
-                int comm= rs.getInt("comm");
-                int deptno = rs.getInt("deptno");
-                
-                Emp emp = new Emp(empno, ename, job, mgr, hiredate, sal, comm, deptno); 
-                list.add(emp);
-            }
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (rs != null) rs.close();
-                if (pstmt != null) pstmt.close();
-                if (conn != null) conn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return list;
-    }
+		} catch (ClassNotFoundException cnfe) {
+			System.out.println("해당 클래스를 찾을 수 없습니다." + cnfe.getMessage());
+		} catch (SQLException se) {
+			System.out.println(se.getMessage());
+		} finally {
+			if (rs != null)
+				try {
+						rs.close();
+				} catch (SQLException e) {
+					System.out.println(e.getMessage());
+				}
+			
+			if (stmt != null)
+				try {
+						stmt.close();
+				} catch (SQLException e) {
+					System.out.println(e.getMessage());
+				}
+			
+			if (conn != null)
+				try {
+					conn.close(); //4단계 : DB연결을 끊는다.
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+		}
+		return list;
+	}
 }
-
- */
